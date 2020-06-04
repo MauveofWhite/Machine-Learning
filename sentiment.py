57f511b659ff860dbf7062a4c96796655b4da843
richard <rxue1045@gmail.com>
2020-06-03 18:48:13 -0700

    fixed

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = "FaYFO6Q0V4vlQCHAyCBxc29HW"
-consumer_secret = 'mM0ltGeN7U65wq7aX1G9KRmd0GwdZyrvlpVRJmHjN9LZSbw4yN'
+consumer_key =
+consumer_secret =
 
-access_token = '1268307797449379840-1ntuNFILaG52PjLn34TdJ79gdisvLz'
-access_token_secret = 'CQ71A7glUY3jniQLrxLfKXlAQ4pnQBJ1teRVMlCnxFXSu'
+access_token =
+access_token_secret =
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
